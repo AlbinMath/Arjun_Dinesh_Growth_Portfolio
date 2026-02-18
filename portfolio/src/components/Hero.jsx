74a6af167cd02eef63f4843095f81a8aac4573f0
Albin Mathew <albinmathewtomo@gmail.com>
2026-02-18 16:27:17 +0530

    Fix case sensitivity for Vercel: Rename Profile.png to profile-pic.png

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profileImg from '../assets/Profile.png';
+import profileImg from '../assets/profile-pic.png';
 
 const Hero = () => {
     return (
